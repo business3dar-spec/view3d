7a0b2feafef4c587096d9a66cac9f62d0f5057f8
business3dar-spec <business3dar@gmail.com>
2026-06-20 05:38:51 +0300

    index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('public', { index: false }));
 app.use('/uploads', express.static('uploads'));
 
 app.use(session({
